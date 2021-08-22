3124061ae17619fcb981fbbc96211ac57eedc422
EvaTu <evatuw@gmail.com>
2021-08-21 21:22:21 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
+
+function scuberGreetingForFeet(distanceData){
   // Write your code here!
+  if(distanceData <= 400){
+    return "This one is on me!";
+  }else if(distanceData > 2000 && distanceData < 2500){
+    return "I will gladly take your thirty bucks.";
+  }else {
+    return "No can do.";
+  }
+
 }
 
-function ternaryCheckCity(){
+
+function ternaryCheckCity(cityData){
   // Write your code here!
+  return cityData === "NYC"? "Ok, sounds good.": "No go."
 }
 
-function switchOnCharmFromTip(){
+
+function switchOnCharmFromTip(customerData){
+switch(customerData){
   // Write your code here!
+  
+  case"generous":
+    return "Thank you so much.";
+    break;
+  case"not as generous":
+    return "Thank you.";
+    break;
+  default:
+    return "Bye.";
+    break;
+
 }
+}
+
